82dbce93c149f5a1546823e14f2dd6a8912050f8
BuildTools <unconfigured@null.spigotmc.org>
2022-08-15 23:26:58 -0600

    Cleanup

--- src/main/java/com/suicidesquid/syncswitch/init/ItemInit.java
+++ src/main/java/com/suicidesquid/syncswitch/init/ItemInit.java
@@ -7,7 +7,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SynchronousSwitches.MODID);
