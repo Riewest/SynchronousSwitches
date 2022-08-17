00d5b4b494a3ec631bb4272f3343729d3b3ed4a1
BuildTools <unconfigured@null.spigotmc.org>
2022-08-16 20:40:04 -0600

    Working Channel Save

--- src/main/java/com/suicidesquid/syncswitch/blocks/BaseSwitchBlock.java
+++ src/main/java/com/suicidesquid/syncswitch/blocks/BaseSwitchBlock.java
@@ -1,21 +1,28 @@
 package com.suicidesquid.syncswitch.blocks;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import com.suicidesquid.syncswitch.data.SwitchData;
 import com.suicidesquid.syncswitch.tiles.BaseSwitchBlockTile;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeverBlock;
@@ -25,12 +32,30 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootContext.Builder;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class BaseSwitchBlock extends LeverBlock{
 
     public BaseSwitchBlock(Block.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        
+        if(placer != null && placer instanceof Player){
+            BlockEntity be =level.getBlockEntity(pos);
+            if (be instanceof BaseSwitchBlockTile tile){
+                tile.setPlayer(((Player)placer).getStringUUID());
+            }
+        }
+
+        super.setPlacedBy(level, pos, state, placer, stack);
     }
 
     @Override
@@ -43,6 +68,31 @@ public class BaseSwitchBlock extends LeverBlock{
 	}
 
     @Override
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        List<ItemStack> drops = new ArrayList<>();
+        BaseSwitchBlockTile tile = (BaseSwitchBlockTile) builder.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
+        if (tile != null){
+            drops.add(createItem(tile.getChannel()));
+        }
+        return drops;
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
+        BaseSwitchBlockTile tile = (BaseSwitchBlockTile) level.getBlockEntity(pos);
+        return createItem(tile.getChannel());
+    }
+
+    private ItemStack createItem(String channel){
+        ItemStack stack = new ItemStack(this, 1);
+        if (channel != null){
+            CompoundTag tag = stack.getOrCreateTag();
+            tag.putString("channel", channel);
+        }
+        return stack;
+    }
+
+    @Override
     public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         if (!world.isClientSide() && hand == InteractionHand.MAIN_HAND){
             BlockEntity tile = world.getBlockEntity(pos);
@@ -52,7 +102,7 @@ public class BaseSwitchBlock extends LeverBlock{
                 SwitchData switchData = SwitchData.get(world);
                 if (player.isShiftKeyDown()){
                     if(switchtile.hasChannel()){
-                        player.sendSystemMessage(Component.literal("Channel: " + switchtile.getChannelDisplay() + " - Active: " + switchData.isActive(switchtile.getChannel())));   
+                        player.sendSystemMessage(Component.literal("Channel: " + switchtile.getChannelDisplay(player.getStringUUID()) + " - Active: " + switchData.isActive(switchtile.getChannel())));   
                     } else {
                         player.sendSystemMessage(Component.literal("No Channel"));
                     }
--- src/main/java/com/suicidesquid/syncswitch/init/ItemInit.java
+++ src/main/java/com/suicidesquid/syncswitch/init/ItemInit.java
@@ -1,15 +1,20 @@
 package com.suicidesquid.syncswitch.init;
 
 import com.suicidesquid.syncswitch.SynchronousSwitches;
+import com.suicidesquid.syncswitch.items.BaseSwitchBlockItem;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SynchronousSwitches.MODID);
+    // public static final RegistryObject<Item> SWITCH_BLOCK_ITEM = ITEMS.register("switch_block",() -> new BaseSwitchBlockItem(BlockInit.SWITCH_BLOCK.get()));
+
+
     public static class ModCreativeTab extends CreativeModeTab {
         public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "syncswitch");
         private ModCreativeTab(int index, String label) {
