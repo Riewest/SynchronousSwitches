2b75dcf19a223657b12efb55bffdece09c335361
BuildTools <unconfigured@null.spigotmc.org>
2022-08-16 21:06:13 -0600

    Added Hovertext

--- src/main/java/com/suicidesquid/syncswitch/items/BaseSwitchBlockItem.java
+++ src/main/java/com/suicidesquid/syncswitch/items/BaseSwitchBlockItem.java
@@ -1,14 +1,19 @@
 package com.suicidesquid.syncswitch.items;
 
+import java.util.List;
+
 import com.suicidesquid.syncswitch.blocks.SwitchBlock;
+import com.suicidesquid.syncswitch.data.SwitchData;
 import com.suicidesquid.syncswitch.init.ItemInit;
 import com.suicidesquid.syncswitch.tiles.BaseSwitchBlockTile;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -43,4 +48,12 @@ public class BaseSwitchBlockItem extends BlockItem{
         return flag;
     }
     
+    @Override
+    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flagIn) {
+        String channel = getChannel(stack);
+        if (channel != null){
+            tooltip.add(Component.literal("Channel: " + channel));
+        }
+    }
+
 }
